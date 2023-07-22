acee109b21689baea58db3a02dcc236e3e4e266e
Austin Potts <austinpotts@Andariels-Air.lan>
2023-07-22 06:53:24 -0400

    first 2

---
+++ Flight-Hedge.iO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flight-Hedge.iO
+//
+//  Created by Austin Potts on 7/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flight-Hedge.iOUITests/Flight_Hedge_iOUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flight_Hedge_iOUITestsLaunchTests.swift
+//  Flight-Hedge.iOUITests
+//
+//  Created by Austin Potts on 7/22/23.
+//
+
+import XCTest
+
+final class Flight_Hedge_iOUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
