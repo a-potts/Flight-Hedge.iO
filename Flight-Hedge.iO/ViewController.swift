553e0530735e96d132d202f08f74d15436db0d21
Austin Potts <austinpotts@Andariels-Air.lan>
2023-07-22 07:37:42 -0400

    Login UI Added

--- Flight-Hedge.iO/ViewController.swift
+++ Flight-Hedge.iO/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var nameView: UIView!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var passwordView: UIView!
+    
+    @IBOutlet weak var loginButtonView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameView.layer.cornerRadius = 10
+        passwordView.layer.cornerRadius = 10
+        loginButtonView.layer.cornerRadius = 10
+        
     }
 
 
+    
+    //Actions
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        //Handle Firebase Login
+    }
+    
 }
 
