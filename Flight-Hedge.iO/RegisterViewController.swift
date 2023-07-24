3102ebaac6fe0ad945973fc4212ce39385530e7c
Austin Potts <austinpotts@Andariels-Air.lan>
2023-07-24 18:13:15 -0400

    Added Register UI

---
+++ Flight-Hedge.iO/RegisterViewController.swift
@@ -0,0 +1,40 @@
+//
+//  RegisterViewController.swift
+//  Flight-Hedge.iO
+//
+//  Created by Austin Potts on 7/24/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var emailView: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var nameView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
